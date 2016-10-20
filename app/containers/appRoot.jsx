09b7b1f0dddc914ad12b58c92face8777a2c980f
j4dream <j4dream@126.com>
2016-10-20 17:49:14 +0800

    update dev & remote react-docker

--- app/containers/appRoot.jsx
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-	module.exports = require('./AppRoot.prod');
-} else {
-	module.exports = require('./AppRoot.dev');
-}
--- app/main.jsx
+++ app/main.jsx
@@ -4,9 +4,23 @@ import ReactDOM from 'react-dom';
 import AppRoot from './containers/AppRoot';
 import configureStore from './store/configureStore';
 
-let store = configureStore();
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+
+import routes from './router';
+
+const store = createStore(
+	rootReducer,
+	applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-	<AppRoot store={store}/>,
+	<Provider store={store}>
+		<Router history={browserHistory} routes={routes}/>
+	</Provider>,
 	document.body.appendChild(document.createElement('div'))
 );
